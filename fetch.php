8298e1c1392b85a99e50f773f8436fc8d9f02682
Wojtekw20 <wojciechwieczorek09@gmail.com>
2026-02-10 19:43:32 +0100

    pobieranie i zapisywanie wiadomości

--- fetch.php
+++ fetch.php
@@ -1,3 +1,8 @@
 <?php
+header("Content-Type: application/json");
 
-?>
+if (!file_exists("messages.json")) {
+    file_put_contents("messages.json", "[]");
+}
+
+echo file_get_contents("messages.json");
--- send.php
+++ send.php
@@ -1,3 +1,24 @@
 <?php
+session_start();
+$user = $_SESSION['user'];
+
+$message = trim($_POST['message']);
+if ($message === "") exit;
+
+$messages = [];
+
+if (file_exists("messages.json")) {
+    $messages = json_decode(file_get_contents("messages.json"), true);
+}
+
+$messages[] = [
+    "user" => $user,
+    "message" => $message,
+    "time" => date("H:i:s")
+];
+
+file_put_contents("messages.json", json_encode($messages));
+
+echo "OK";
 
 ?>
